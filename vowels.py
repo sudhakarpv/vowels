9451253acb1a6056b698b643709567f00f113c14
pvs <40256682+sudhakarpv@users.noreply.github.com>
2018-07-24 19:25:18 +0530

    Update vowels.py

--- vowels.py
+++ vowels.py
@@ -2,14 +2,16 @@
 def main():
     pass
     n=input().lower()
+    l=list(n)
     temp=int(0)
-    for i in n:
+    for i in l:
         if(i=="a" or i=="e" or i=="i" or i=="o" or i=="u"):
             temp+=1
-    if(temp>1):
+    if(temp>0):
         print("yes")
     else:
         print("no")
+
 
 if __name__ == '__main__':
     main()
